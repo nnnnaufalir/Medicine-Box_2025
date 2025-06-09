7b820b4407f89018a4303562e26f074ea10dc374
nnnnaufalir <ilhamnaufalr29@gmail.com>
2025-06-10 02:31:52 +0700

    testing upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,22 @@
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(115200);
   int result = myFunction(2, 3);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  Serial.println(myFunction(5, 7)); // Example usage of myFunction
+  delay(1000);                      // Delay for readability
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
