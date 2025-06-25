bb3061d5e837fca91e9d2b4418c93e8f7b236a83
nnnnaufalir <ilhamnaufalr29@gmail.com>
2025-06-25 12:05:38 +0700

    update HardwareConfig.h

--- include/HardwareConfig.h
+++ include/HardwareConfig.h
@@ -1,31 +1,30 @@
 #ifndef HARDWARE_CONFIG_H
 #define HARDWARE_CONFIG_H
 
-// --- Pin Konfigurasi Hardware ---
+// --- Hardware Pin Configuration ---
 
-// Buzzer
 #define BUZZER_PIN 32
 
-// TFT Display (ILI9341)
-#define TFT_MOSI_PIN 23 // SPI MOSI (Master Out Slave In)
-#define TFT_MISO_PIN 19 // SPI MISO (Master In Slave Out)
-#define TFT_SCLK_PIN 18 // SPI SCLK (Serial Clock)
-#define TFT_DC_PIN 25   // Data/Command (DC)
-#define TFT_CS_PIN 26   // Chip Select (CS)
-#define TFT_RST_PIN 27  // Reset (RST)
-#define TFT_LED_PIN 33  // Pin untuk kontrol Backlight LED TFT
+// TFT Display (ILI9341) Pins
+#define TFT_MOSI_PIN 23
+#define TFT_MISO_PIN 19
+#define TFT_SCLK_PIN 18
+#define TFT_DC_PIN 25
+#define TFT_CS_PIN 26
+#define TFT_RST_PIN 27
+#define TFT_LED_PIN 33
 
-// Sensor & Input
-#define BATT_MONITOR_PIN 34 // Pin untuk monitoring tegangan baterai (ADC)
-#define LOCK_CHECKER_PIN 35 // Pin untuk mendeteksi status kunci
-#define PB1_PIN 39          // Push Button 1
-#define PB2_PIN 36          // Push Button 2
+// Sensor & Input Pins
+#define BATT_MONITOR_PIN 34
+#define LOCK_CHECKER_PIN 35
+#define PB1_PIN 39
+#define PB2_PIN 36
 
-// HX711 Load Cell
-#define HX711_SCK_PIN 14 // Serial Clock untuk HX711 (SCK)
-#define HX711_DT_PIN 13  // Data pin untuk HX711 (DOUT)
+// HX711 Load Cell Pins
+#define HX711_SCK_PIN 14
+#define HX711_DT_PIN 13
 
-// --- Konfigurasi Umum Hardware ---
-#define SERIAL_BAUD_RATE 115200 // Baud rate untuk Serial Monitor
+// --- General Hardware Configuration ---
+#define SERIAL_BAUD_RATE 115200
 
 #endif // HARDWARE_CONFIG_H
