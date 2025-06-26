e48db910740a1223a6a929afc64e718195cb0a49
nnnnaufalir <ilhamnaufalr29@gmail.com>
2025-06-26 17:30:06 +0700

    adjust function now

--- lib/RTC_Module/RTC_Module.cpp
+++ lib/RTC_Module/RTC_Module.cpp
@@ -4,42 +4,22 @@ RTC_Module::RTC_Module() {}
 
 bool RTC_Module::begin()
 {
-    // Hanya inisialisasi RTC
     if (!_rtc.begin())
     {
         Serial.println("Kesalahan: RTC tidak ditemukan!");
         return false;
     }
 
+    // Atur waktu ke waktu kompilasi jika RTC kehilangan daya
     if (_rtc.lostPower())
     {
         Serial.println("Info: RTC kehilangan daya, waktu diatur.");
         _rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
-    return true; // Selalu kembalikan true jika RTC ditemukan
+    return true;
 }
 
 DateTime RTC_Module::now()
 {
     return _rtc.now();
-}
-
-void RTC_Module::adjust(const DateTime &dt)
-{
-    _rtc.adjust(dt);
-}
-
-String RTC_Module::getFormattedTime(bool withSeconds)
-{
-    DateTime now = _rtc.now();
-    char buf[10];
-    if (withSeconds)
-    {
-        snprintf(buf, sizeof(buf), "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
-    }
-    else
-    {
-        snprintf(buf, sizeof(buf), "%02d:%02d", now.hour(), now.minute());
-    }
-    return String(buf);
 }
--- lib/RTC_Module/RTC_Module.h
+++ lib/RTC_Module/RTC_Module.h
@@ -2,7 +2,6 @@
 #define RTC_MODULE_H
 
 #include <RTClib.h>
-// #include "Adafruit_EEPROM_I2C.h" // Tidak kita perlukan untuk sekarang
 
 class RTC_Module
 {
@@ -10,16 +9,9 @@ public:
     RTC_Module();
     bool begin();
     DateTime now();
-    String getFormattedTime(bool withSeconds = true);
-    void adjust(const DateTime &dt);
-
-    // Fungsi EEPROM kita nonaktifkan sementara
-    // void writeFloat(uint16_t address, float value);
-    // float readFloat(uint16_t address);
 
 private:
     RTC_DS3231 _rtc;
-    // Adafruit_EEPROM_I2C _eeprom; // Nonaktifkan
 };
 
 #endif // RTC_MODULE_H
